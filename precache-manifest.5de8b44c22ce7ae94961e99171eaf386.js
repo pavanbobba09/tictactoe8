c3199afa9681694460ad48a42ddfb48d1e6d9ffe
pavanbobba09 <pavanbobba2000@gmail.com>
2024-11-18 17:50:25 -0500

    Updates

---
+++ precache-manifest.5de8b44c22ce7ae94961e99171eaf386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ff804e39ded937deff81b7e2a34746",
+    "url": "/tictactoe2/index.html"
+  },
+  {
+    "revision": "03ccf2994956badc9ad2",
+    "url": "/tictactoe2/static/css/main.d118b03b.chunk.css"
+  },
+  {
+    "revision": "53773187aad2c36af482",
+    "url": "/tictactoe2/static/js/2.17a70194.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/tictactoe2/static/js/2.17a70194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ccf2994956badc9ad2",
+    "url": "/tictactoe2/static/js/main.d6775831.chunk.js"
+  },
+  {
+    "revision": "b4d8ac2709c3524d6ec8",
+    "url": "/tictactoe2/static/js/runtime-main.ac3f1b02.js"
+  }
+]);
